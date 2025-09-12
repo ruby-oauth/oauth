5dd8a765240718ac025364001a66c2128e042a03
Peter H. Boling <peter.boling@gmail.com>
2025-09-12 10:56:05 -0600

    💚 Fixing old Rubies

--- lib/oauth/helper.rb
+++ lib/oauth/helper.rb
@@ -18,11 +18,11 @@ module OAuth
     end
 
     def _escape(string)
-      URI::RFC2396_PARSER.escape(string, OAuth::RESERVED_CHARACTERS)
+      URI::DEFAULT_PARSER.escape(string, OAuth::RESERVED_CHARACTERS)
     end
 
     def unescape(value)
-      URI::RFC2396_PARSER.unescape(value.gsub("+", "%2B"))
+      URI::DEFAULT_PARSER.unescape(value.gsub("+", "%2B"))
     end
 
     # Generate a random key of up to +size+ bytes. The value returned is Base64 encoded with non-word
